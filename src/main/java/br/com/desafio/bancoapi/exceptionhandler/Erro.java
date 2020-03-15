80ba7821dda7889cca9d4ca494fb872b5971be14
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-03-15 19:57:58 -0300

    Adicionado classe para capturar exceções e lançar mensagens padronizadas

---
+++ src/main/java/br/com/desafio/bancoapi/exceptionhandler/Erro.java
@@ -0,0 +1,29 @@
+package br.com.desafio.bancoapi.exceptionhandler;
+
+public class Erro {
+
+  private String mensagemUsuario;
+  private String mensagemDesenvolvedor;
+
+  public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+    this.mensagemUsuario = mensagemUsuario;
+    this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+  }
+
+  public String getMensagemUsuario() {
+    return mensagemUsuario;
+  }
+
+  public void setMensagemUsuario(String mensagemUsuario) {
+    this.mensagemUsuario = mensagemUsuario;
+  }
+
+  public String getMensagemDesenvolvedor() {
+    return mensagemDesenvolvedor;
+  }
+
+  public void setMensagemDesenvolvedor(String mensagemDesenvolvedor) {
+    this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+  }
+
+}
