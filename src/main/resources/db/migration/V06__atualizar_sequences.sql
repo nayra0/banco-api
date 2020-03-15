7127acaacd2a1e69f2554b32c2755248d551e26c
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-03-15 00:25:15 -0300

    Adicionado arquivo de migração para atualizar as sequences

---
+++ src/main/resources/db/migration/V06__atualizar_sequences.sql
@@ -0,0 +1,5 @@
+alter sequence banco_seq restart with 3;
+alter sequence agencia_seq restart with 3;
+alter sequence cliente_seq restart with 3;
+alter sequence conta_seq restart with 2;
+alter sequence transacao_seq restart with 2;
